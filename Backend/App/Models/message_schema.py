0091f0e6edda07f7c8b79fc2acda73f126dad307
that1Kid878 <a.anaqi2010@gmail.com>
2026-01-03 18:07:05 +0800

    Add: Database models (Models)

---
+++ Backend/App/Models/message_schema.py
@@ -0,0 +1,34 @@
+from sqlalchemy import (
+    Column,
+    UUID,
+    Text,
+    DateTime,
+    ForeignKey,
+)
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class Message:
+    message_id = Column(UUID(as_uuid=True), primary_key=True)
+    sender_id = Column(
+        UUID(as_uuid=True),
+        ForeignKey("users.user_id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    reciever_id = Column(
+        UUID(as_uuid=True),
+        ForeignKey("users.user_id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    body = Column(Text, nullable=False)
+    created_at = Column(DateTime(timezone=True))
+
+
+class Offline_Message(Message, Base):
+    __tablename__ = "offline_messages"
+
+
+class Chat_Log(Message, Base):
+    __tablename__ = "chat_log"
---
+++ Backend/App/Models/user_schema.py
@@ -0,0 +1,20 @@
+from sqlalchemy import (
+    Column,
+    UUID,
+    Text,
+    DateTime,
+    VARCHAR,
+)
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(UUID(as_uuid=True), primary_key=True)
+    username = Column(VARCHAR(50), unique=True, nullable=False)
+    hashed_password = Column(Text, nullable=False)
+    email = Column(Text, unique=True, nullable=False)
+    created_at = Column(DateTime(timezone=True))
