b56b2b1f6c93a156f839223eae148fa56390cbd3
that1Kid878 <a.anaqi2010@gmail.com>
2026-01-05 21:07:06 +0800

    Minor changes

--- Backend/App/Repositories/user_repo.py
+++ Backend/App/Repositories/user_repo.py
@@ -1,6 +1,5 @@
 from Backend.App.Models.user_schema import User_Model
 from Backend.App.Core.exceptions import (
-    Missing_Parameters,
     Invalid_Parameters,
     Integrity_Error_Handler,
 )
@@ -9,16 +8,13 @@ from sqlalchemy.exc import IntegrityError
 from uuid import UUID
 
 
-def Get_User(
-    db: Session, user_id: UUID = None, username: str = None
-) -> User_Model | None:
-    if user_id and username is None:
-        raise Missing_Parameters()
-    if user_id:
-        User = db.query(User_Model).filter(User_Model.user_id == user_id).first()
-    elif username:
-        User = db.query(User_Model).filter(User_Model.username == username).first()
+def Get_User_By_Id(db: Session, user_id: UUID) -> User_Model | None:
+    User = db.query(User_Model).filter(User_Model.user_id == user_id).first()
+    return User
 
+
+def Get_User_By_Username(db: Session, username: str) -> User_Model | None:
+    User = db.query(User_Model).filter(User_Model.username == username).first()
     return User
 
 
@@ -28,15 +24,16 @@ def Create_User(db: Session, User: User_Model):
         db.commit()
         db.refresh(User)
     except IntegrityError as error:
-        pg_error = error.orig
-        pg_code = getattr(pg_error, "pgcode")
+        db.rollback()
+        pg_code = getattr(error.orig, "pgcode", None)
         raise Integrity_Error_Handler(pg_code)
 
 
-def Delete_User(db: Session, user_id: UUID = None, username: str = None):
-    User = Get_User(db, user_id, username)
+def Delete_User(db: Session, user_id: UUID = None):
+    User = Get_User_By_Id(db, user_id)
     if User:
         db.delete(User)
         db.commit()
     else:
+        db.rollback()
         raise Invalid_Parameters()
