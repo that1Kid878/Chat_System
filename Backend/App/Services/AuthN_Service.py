482f126af5bdb1b908ecd2c276bc02faad2573fd
that1Kid878 <a.anaqi2010@gmail.com>
2026-01-08 22:01:44 +0800

    Rough planning

---
+++ Backend/App/Services/AuthN_Service.py
@@ -0,0 +1,33 @@
+# Login
+# Logout
+# create user
+# Delete user
+# Get user
+
+
+def Login():
+    # Check username_password
+    # Generate access and refresh
+    # Return
+    ...
+
+
+def Logout():
+    # Check access
+    # Remove refresh from db
+    ...
+
+
+def Create_User():
+    # Add to db
+    ...
+
+
+def Get_User():
+    # Check db
+    ...
+
+
+def Delete_User():
+    # Remove user from db
+    ...
