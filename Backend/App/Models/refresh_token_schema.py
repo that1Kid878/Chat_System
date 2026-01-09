33e4cc24d0a46b93c0905a7f6d6c369820f2df37
that1Kid878 <a.anaqi2010@gmail.com>
2026-01-09 22:36:57 +0800

    Remove default

--- Backend/App/Models/refresh_token_schema.py
+++ Backend/App/Models/refresh_token_schema.py
@@ -2,18 +2,12 @@ from sqlalchemy import Text, DateTime, Boolean, ForeignKey, text
 from sqlalchemy.orm import mapped_column
 from Backend.App.Core.database import Base
 from Backend.App.Core.types import GUID
-import uuid
 
 
 class Refresh_Token_Model(Base):
     __tablename__ = "refresh_tokens"
 
-    token_id = mapped_column(
-        GUID(),
-        primary_key=True,
-        default=uuid.uuid4(),
-        server_default=text("gen_random_uuid()"),
-    )
+    token_id = mapped_column(GUID(), primary_key=True)
     user_id = mapped_column(
         GUID(),
         ForeignKey("users.user_id", ondelete="CASCADE"),
