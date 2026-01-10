4be8120c57ea0884d0080915705105fc8a80efb8
that1Kid878 <a.anaqi2010@gmail.com>
2026-01-10 21:07:09 +0800

    Function rename

--- Backend/Test/Integration/test_user_repo.py
+++ Backend/Test/Integration/test_user_repo.py
@@ -1,7 +1,7 @@
 from Backend.App.Repositories.user_repo import (
-    Create_User,
+    Create_Database_User,
     Get_User_By_Username,
-    Delete_User,
+    Delete_Database_User,
 )
 from Backend.App.Models.user_schema import User_Model
 
@@ -12,13 +12,13 @@ def test_user_creation(db):
     User.email = "nice@gmail.com"
     User.hashed_password = "psoijsdvkosijdsasjf"
 
-    Create_User(db, User)
+    Create_Database_User(db, User)
 
     DB_User = Get_User_By_Username(db, username="that1Kid")
     assert DB_User.username == User.username
     assert DB_User.email == User.email
     assert DB_User.hashed_password == User.hashed_password
 
-    Delete_User(db, DB_User.user_id)
+    Delete_Database_User(db, DB_User.user_id)
 
     assert Get_User_By_Username(db, username="that1Kid") is None
